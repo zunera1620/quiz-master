b727f9353e468f4d4f57f7b7b75cef3af602b8bf
zunera1620 <zunera1162007@gmail.com>
2026-09-20 20:24:25 +0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base: '/quiz-master/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
